d1292c91f03a66608f4ba9d6779ba29e6c0f035a
isaac <isaac_mr.g@hotmail.com>
2022-05-16 11:51:41 +0930

    function working

--- player.cpp
+++ player.cpp
@@ -4,15 +4,15 @@ Player::Player(){}
 
 void Player::setSpeed(int speed) { this->speed = speed; }
 void Player::setStrength(int strength) { this->strength = strength; }
-void Player::setHp(int _hp) { this->hp = _hp; }
+void Player::setHP(int _hp) { this->hp = _hp; }
 
 
 void Player::applyCharacter(Character character)
 {
     setSpeed(character.getSpeed());
     setStrength(character.getStrength());
-    setHP(character.getHp());
-    this->full_hp = character.getHp();
+    setHP(character.getHP());
+    this->full_hp = character.getHP();
 }
 
 Player::~Player() {}
