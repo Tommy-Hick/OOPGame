b8bf037528f0a6ccc0ab4d1a770428497050f790
Tommy-Hick <a1825267@adelaide.edu.au>
2022-05-16 20:43:05 +0930

    hopefully everything is ok

--- Character.h
+++ Character.h
@@ -17,8 +17,9 @@ protected:
     int strength; //How much damage a player is able to deal to another character
     int speed; //Determines the speed of a player, where faster players attack first
 public:
-    Character();
-    Character(std::string name,int hp,int strength,int speed);
+    Character(); //Default constructor for character object
+    Character(std::string name,int hp,int strength,int speed); //Constructor for Character that takes in the name, health, strength and speed of
+                                                                
     std::string getName();
     
     int getHp();
